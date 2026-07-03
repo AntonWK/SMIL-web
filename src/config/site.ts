760913dbb8c6e4cdf8dc464d6ca146ea095a8168
Anton B <anton.wk.berglund@gmail.com>
2026-07-03 17:00:49 +0200

    start

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,35 +1,45 @@
 import ogImage from "@/assets/og-image.png";
 
 export const siteConfig = {
-  name: "Astro Starter Pro",
+  name: "Smil",
   description:
-    "Starter template optimized for SEO and performance. A solid foundation to start your projects with best practices.",
-  url: "https://astrostarterpro.com",
-  lang: "en",
-  locale: "en_US",
-  author: "Devgelo",
-  twitter: "@Devgelo",
+    "Utveckla ditt företag och ledarskap med SMIL i Linköping. Få tillgång till ett unikt nätverk, givande utbildningar och värdefulla affärskontakter.",
+  url: "https://smil.se",
+  lang: "sv",
+  locale: "sv_SE",
+  author: "SMIL", //Vad för namn ska stå som avsändare på sidan? (ex. Smil, Smil AB, Smil Företagsutveckling AB)
+  facebook: "https://www.facebook.com/smilforetagsutveckling/",
   ogImage: ogImage,
   socialLinks: {
-    twitter: "https://twitter.com",
-    github: "https://github.com/devgelo-labs/astro-starter-pro",
-    discord: "https://discord.com",
+    linkedin: "https://se.linkedin.com/company/smilse/",
+    facebook: "https://www.facebook.com/smilforetagsutveckling/",
   },
   navLinks: [
-    { text: "Home", href: "/" },
-    { text: "About", href: "/about" },
-    { text: "Services", href: "/services" },
-    { text: "Pricing", href: "/pricing" },
-    { text: "Blog", href: "/blog" },
-    { text: "Contact", href: "/contact" },
-    { text: "Widgets", href: "/widgets" },
+    { text: "Hem", href: "/" },
     {
-      text: "Templates",
-      href: "/templates",
+      text: "Utbildningar",
+      href: "/utbildningar",
       links: [
-        { text: "Personal Portfolio", href: "/templates/portfolio" },
-        { text: "SaaS Landing", href: "/templates/saas" },
+        {
+          text: "Managementprogram i retorik",
+          href: "/utbildningar/managementprogram-i-retorik",
+        },
+        {
+          text: "Utvecklingsprogrammet",
+          href: "/utbildningar/utvecklingsprogrammet",
+        },
       ],
     },
+    {
+      text: "Evenemang",
+      href: "/evenemang",
+      links: [
+        { text: "Smilpub", href: "/evenemang/smilpub" },
+        { text: "Smil-konferens", href: "/evenemang/smil-konferens" },
+      ],
+    },
+    { text: "Medlemmar", href: "/medlemmar" },
+    { text: "Bli medlem", href: "/bli-medlem" },
+    { text: "Om SMIL", href: "/om-smil" },
   ],
 };
